e4d0641ead2d4b7dff3a3cc0dc26a62567179a91
pelzang <pelzangtp@gmail.com>
2025-09-20 16:22:48 +0600

    updated

--- model/error.go
+++ model/error.go
@@ -1,11 +1,5 @@
 package model
 
-type AuthError struct {
-    Message string `json:"message"`
-    Code    string `json:"code"`
-    Field   string `json:"field,omitempty"`
-}
-
 type UserError struct {
     Message string `json:"message"`
     Code    string `json:"code"`
