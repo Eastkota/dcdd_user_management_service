48486fab711364ddcf88c053bfe5190de2ce46b7
pelzang <pelzangtp@gmail.com>
2025-09-20 15:22:27 +0600

    updated user profile result

--- graph/user.go
+++ graph/user.go
@@ -83,8 +83,8 @@ var UserResult = graphql.NewObject(graphql.ObjectConfig{
 	},
 })
 
-var UserProfileResult = graphql.NewObject(graphql.ObjectConfig{
-	Name: "UserProfileResult",
+var DcddUserProfileResult = graphql.NewObject(graphql.ObjectConfig{
+	Name: "DcddUserProfileResult",
 	Fields: graphql.Fields{
 		"user_profile": &graphql.Field{Type: UserProfile},
 	},
